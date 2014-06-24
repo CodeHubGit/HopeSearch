473b12e54dbdf1cc446a4c2ffba1d56e57910ea2
Max Johansen <macsj200@gmail.com>
2014-06-24 13:40:04 -0600

    actually get filter params working

--- public/modules/prescriptions/controllers/prescriptions.client.controller.js
+++ public/modules/prescriptions/controllers/prescriptions.client.controller.js
@@ -2,29 +2,29 @@
 
 // Prescriptions controller
 angular.module('prescriptions').controller('PrescriptionsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Prescriptions',
-	function($scope, $stateParams, $location, Authentication, Prescriptions ) {
-		$scope.authentication = Authentication;
+    function($scope, $stateParams, $location, Authentication, Prescriptions ) {
+        $scope.authentication = Authentication;
 
-		// Create new Prescription
-		$scope.create = function() {
-			// Create new Prescription object
-			var prescription = new Prescriptions ({
-				eyes: [
+        // Create new Prescription
+        $scope.create = function() {
+            // Create new Prescription object
+            var prescription = new Prescriptions ({
+                eyes: [
                     {sphere: this.rSphere, cylinder:this.rCylinder, axis:this.rAxis, position:'Right'},
                     {sphere: this.lSphere, cylinder:this.lCylinder, axis:this.lAxis, position:'Left'}
                 ],
                 number:this.number
-			});
+            });
 
-			// Redirect after save
-			prescription.$save(function(response) {
-				$location.path('prescriptions/' + response._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
+            // Redirect after save
+            prescription.$save(function(response) {
+                $location.path('prescriptions/' + response._id);
+            }, function(errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
 
-			// Clear form fields
-			this.eyes = [];
+            // Clear form fields
+            this.eyes = [];
             this.rSphere = '';
             this.rCylinder = '';
             this.rAxis = '';
@@ -32,50 +32,59 @@ angular.module('prescriptions').controller('PrescriptionsController', ['$scope',
             this.lAxis = '';
             this.lCylinder = '';
             this.number = '';
-		};
+        };
 
-		// Remove existing Prescription
-		$scope.remove = function( prescription ) {
-			if ( prescription ) { prescription.$remove();
+        // Remove existing Prescription
+        $scope.remove = function( prescription ) {
+            if ( prescription ) { prescription.$remove();
 
-				for (var i in $scope.prescriptions ) {
-					if ($scope.prescriptions [i] === prescription ) {
-						$scope.prescriptions.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.prescription.$remove(function() {
-					$location.path('prescriptions');
-				});
-			}
-		};
+                for (var i in $scope.prescriptions ) {
+                    if ($scope.prescriptions [i] === prescription ) {
+                        $scope.prescriptions.splice(i, 1);
+                    }
+                }
+            } else {
+                $scope.prescription.$remove(function() {
+                    $location.path('prescriptions');
+                });
+            }
+        };
 
-		// Update existing Prescription
-		$scope.update = function() {
-			var prescription = $scope.prescription ;
+        // Update existing Prescription
+        $scope.update = function() {
+            var prescription = $scope.prescription ;
 
-			prescription.$update(function() {
-				$location.path('prescriptions/' + prescription._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+            prescription.$update(function() {
+                $location.path('prescriptions/' + prescription._id);
+            }, function(errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-		// Find a list of Prescriptions
-		$scope.find = function() {
-			$scope.prescriptions = Prescriptions.query();
-		};
-
-        // search for a list of Prescriptions
-        $scope.search = function() {
+        // Find a list of Prescriptions
+        $scope.find = function() {
             $scope.prescriptions = Prescriptions.query();
         };
 
-		// Find existing Prescription
-		$scope.findOne = function() {
-			$scope.prescription = Prescriptions.get({
-				prescriptionId: $stateParams.prescriptionId
-			});
-		};
-	}
+        // search for a list of Prescriptions
+        $scope.search = function() {
+            $scope.prescriptions = Prescriptions.query({prescription:
+            {
+                eyes: [
+                    {sphere: this.rSphere, cylinder:this.rCylinder, axis:this.rAxis, position:'Right'},
+                    {sphere: this.lSphere, cylinder:this.lCylinder, axis:this.lAxis, position:'Left'}
+                ],
+                number:this.number
+            }
+
+            });
+        };
+
+        // Find existing Prescription
+        $scope.findOne = function() {
+            $scope.prescription = Prescriptions.get({
+                prescriptionId: $stateParams.prescriptionId
+            });
+        };
+    }
 ]);
